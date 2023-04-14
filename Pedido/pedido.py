4c145daab58a6d732bb90ad6d1e970fc9cab77cb
Gilvaney Leandro <GILVANEY.LEANDRO@UFRPE.BR>
2023-04-14 12:38:26 -0300

    att pedido e usuario

--- Pedido/pedido.py
+++ Pedido/pedido.py
@@ -1,16 +1,26 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from typing import List
+import mysql.connector
 
 # Importar as classes Produto e Cliente dos módulos correspondentes
 from produto import Produto
 from cliente import Cliente
 
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='quickstore',
+)
 
-app = Flask(__name__)
+app = Flask(name)
 
 
 class Pedido:
-    def __init__(self, idpedido: int, produtos: List[Produto], cliente: Cliente):
+    def init(self, idpedido: int, produtos: List[Produto], cliente: Cliente):
+        mycursor = mydb.cursor()
+        mycursor.execute('SELECT * FROM Produto')
+        meus_pedidos = mycursor.fetchall()
         self.idpedido = idpedido
         self.produtos = produtos
         self.cliente = cliente
@@ -36,35 +46,62 @@ def buscar_pedido(idpedido):
 @app.route('/pedidos', methods=['POST'])
 def criar_pedido():
     data = request.json
-    idpedido = data['idpedido']
-    produtos = [Produto(p['idproduto'], p['nome'], p['preco']) for p in data['produtos']]
-    cliente = Cliente(data['cliente']['idcliente'], data['cliente']['nome'])
-    pedido = Pedido(idpedido, produtos, cliente)
-    pedidos.append(pedido)
-    return jsonify({'message': 'Pedido criado com sucesso!'})
+    mycursor = mydb.cursor()
+    sql = f"insert into produto(idpedido, produtos, cliente) values  ('{data['idpedido']}',{data['produtos']},{data['cliente']});"
+    mycursor.execute(sql)
+    mydb.commit()
+    return make_response(
+       jsonify(
+           mensagem='Feito com sucesso',
+       )
+   )
+    #data = request.json
+    #idpedido = data['idpedido']
+    #produtos = [Produto(p['idproduto'], p['nome'], p['preco']) for p in data['produtos']]
+    #cliente = Cliente(data['cliente']['idcliente'], data['cliente']['nome'])
+    #pedido = Pedido(idpedido, produtos, cliente)
+    #pedidos.append(pedido)
+    #return jsonify({'message': 'Pedido criado com sucesso!'})
 
 
 # Atualizar um pedido existente
 @app.route('/pedidos/<int:idpedido>', methods=['PUT'])
 def atualizar_pedido(idpedido):
-    pedido = buscar_pedido(idpedido)
-    if pedido:
-        data = request.json
-        produtos = [Produto(p['idproduto'], p['nome'], p['preco']) for p in data['produtos']]
-        cliente = Cliente(data['cliente']['idcliente'], data['cliente']['nome'])
-        pedido.produtos = produtos
-        pedido.cliente = cliente
-        return jsonify({'message': 'Pedido atualizado com sucesso!'})
-    else:
-        return jsonify({'message': 'Pedido não encontrado.'}), 404
+    data = request.json
+    idpedido = data['idpedido']
+    produtos = data['produtos']
+    cliente = data['cliente']
+    mycursor1 = mydb.cursor()
+    sql1 = f"UPDATE pedido SET idpedido, produtos, cliente = ('{data['idpedido']}',{data['produtos']},{data['cliente']});"
+    mycursor1.execute(sql1)
+    mydb.commit()
+    return make_response
+
+    #pedido = buscar_pedido(idpedido)
+   # if pedido:
+   #     data = request.json
+   #     produtos = [Produto(p['idproduto'], p['nome'], p['preco']) for p in data['produtos']]
+   #     cliente = Cliente(data['cliente']['idcliente'], data['cliente']['nome'])
+   #     pedido.produtos = produtos
+    #    pedido.cliente = cliente
+   #     return jsonify({'message': 'Pedido atualizado com sucesso!'})
+   # else:
+   #     return jsonify({'message': 'Pedido não encontrado.'}), 404
 
 
 # Deletar um pedido existente
 @app.route('/pedidos/<int:idpedido>', methods=['DELETE'])
 def deletar_pedido(idpedido):
-    pedido = buscar_pedido(idpedido)
-    if pedido:
-        pedidos.remove(pedido)
-        return jsonify({'message': 'Pedido deletado com sucesso!'})
-    else:
-        return jsonify({'message': 'Pedido não encontrado.'}), 404
+    data = request.json
+    idpedido = data['idpedido']
+    mycursor = mydb.cursor()
+    sql = f"DELETE FROM produto WHERE id = ('{data['id']}')"
+    mycursor.execute(sql)
+    mydb.commit()
+    return make_response()
+    #pedido = buscar_pedido(idpedido)
+    #if pedido:
+    #    pedidos.remove(pedido)
+     #   return jsonify({'message': 'Pedido deletado com sucesso!'})
+    #else:
+     #   return jsonify({'message': 'Pedido não encontrado.'}), 404
--- Usuário/user.py
+++ Usuário/user.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,  jsonify, make_response
+from typing import List
+import mysql.connector
 
-app = Flask(__name__)
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='quickstore',
+)
+
+app = Flask(name)
 
 users = [
     {
         "id": 1,
-        "nome": "João da Silva",
+        "name": "João da Silva",
         "cargo": "Vendedor",
         "senha": "123456"
     },
     {
         "id": 2,
-        "nome": "Maria Souza",
+        "name": "Maria Souza",
         "cargo": "Marketing",
         "senha": "abcdef"
     }
@@ -28,28 +38,44 @@ def get_user(user_id):
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    new_user = {
-        "id": users[-1]['id'] + 1,
-        "nome": request.json['nome'],
-        "cargo": request.json['cargo'],
-        "senha": request.json['senha']
-    }
-    users.append(new_user)
-    return jsonify(new_user)({"message": "Usuario criado!"})
+    data = request.json
+    mycursor = mydb.cursor()
+    sql = f"insert into user(id, nome, cargo, senha) values  ('{data['id']}',{data['nome']},{data['cargo']},{data['senha']});"
+    mycursor.execute(sql)
+    mydb.commit()
+    return make_response(
+       jsonify(
+           mensagem='Usuario criado com sucesso',
+       )
+   )
 
 @app.route('/users/<int:user_id>', methods=['PUT'])
 def update_user(user_id):
-    user = [user for user in users if user['id'] == user_id]
-    user[0]['nome'] = request.json.get('nome', user[0]['nome'])
-    user[0]['cargo'] = request.json.get('cargo', user[0]['cargo'])
-    user[0]['senha'] = request.json.get('senha', user[0]['senha'])
-    return jsonify(user[0])
+    data = request.json
+    user_id = data['id']
+    nome = data['nome']
+    cargo = data['cargo']
+    senha = data['senha']
+    mycursor1 = mydb.cursor()
+    sql1 = f"UPDATE usuário SET nome, cargo, senha, id = ('{data['nome']}',{data['cargo']},{data['senha']})  WHERE id = ('{data['id']}');"
+    mycursor1.execute(sql1)
+    mydb.commit()
+    return make_response
+    #user = [user for user in users if user['id'] == user_id]
+    #user[0]['nome'] = request.json.get('nome', user[0]['nome'])
+    #user[0]['cargo'] = request.json.get('cargo', user[0]['cargo'])
+    #user[0]['senha'] = request.json.get('senha', user[0]['senha'])
+    #return jsonify(user[0])
 
 @app.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    user = [user for user in users if user['id'] == user_id]
-    users.remove(user[0])
-    return jsonify({"message": "Usuario deletado."})
+    data = request.json
+    user_id = data['id']
+    mycursor = mydb.cursor()
+    sql = f"DELETE FROM produto WHERE id = ('{data['id']}')"
+    mycursor.execute(sql)
+    mydb.commit()
+    return make_response()
 
-if __name__ == '__main__':
+if name == 'main':
     app.run(debug=True)
